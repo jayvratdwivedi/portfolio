338571be2fcae231071f98d230be1bc4bc3cc95c
Jayvrat Dwivedi <jayvratdwivedi202@gmail.com>
2025-07-19 17:15:12 +0530

    add Skills and fix css, background

---
+++ components/sub/SkillDataProvider.jsx
@@ -0,0 +1,36 @@
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+const SkillDataProvider = ({ src, name, width, height, index }) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+  });
+
+  const imageVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+
+  const animationDelay = 0.1;
+
+  return (
+    <motion.div
+      ref={ref}
+      initial="hidden"
+      variants={imageVariants}
+      animate={inView ? "visible" : "hidden"}
+      custom={index}
+      transition={{ delay: index * animationDelay }}
+    >
+      <img
+        src={`/skills/${src}`}
+        width={width}
+        height={height}
+        alt={name}
+        loading="lazy"
+      />
+    </motion.div>
+  );
+};
+
+export default SkillDataProvider;
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import Hero from "../components/main/Hero";
+import Skills from "../components/main/Skills";
+import StarsCanvas from "../components/main/StarCanvas";
 import "./App.css";
 import "@fontsource/inter";
 
 function App() {
   return (
-    <div className=" text-white font-sans overflow-x-hidden overflow-y-scroll min-h-screen">
+    <div className="relative w-full min-h-screen text-white font-sans overflow-x-hidden overflow-y-auto">
+      <div className="fixed inset-0 bg-[#030014] -z-20" />
+      <div className="fixed inset-0 -z-10 pointer-events-none">
+        <StarsCanvas />
+      </div>
       <main className="h-full w-full">
         <div className="flex flex-col gap-20">
           <Hero />
+          <Skills />
         </div>
       </main>
     </div>
