836900e49058c1845eb2c38e038734f33a4ddaa0
Jayvrat Dwivedi <jayvratdwivedi202@gmail.com>
2025-07-19 15:45:09 +0530

    add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import Hero from "../components/main/Hero";
 import "./App.css";
 
 function App() {
   return (
     <main className="h-full w-full">
       <div className="flex flex-col gap-20">
-        <h1 className="text-4xl font-bold text-blue-600">Hello</h1>
-        <h2 className="text-2xl text-gray-500">World</h2>
+        <Hero />
       </div>
     </main>
   );
