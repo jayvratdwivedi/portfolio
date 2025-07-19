aee6723d1b779acd63e93c082c149a59e7e4869d
Jayvrat Dwivedi <jayvratdwivedi202@gmail.com>
2025-07-19 16:38:01 +0530

    update code and add few files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Hero from "../components/main/Hero";
 import "./App.css";
+import "@fontsource/inter";
 
 function App() {
   return (
-    <main className="h-full w-full">
-      <div className="flex flex-col gap-20">
-        <Hero />
-      </div>
-    </main>
+    <div className=" text-white font-sans overflow-x-hidden overflow-y-scroll min-h-screen">
+      <main className="h-full w-full">
+        <div className="flex flex-col gap-20">
+          <Hero />
+        </div>
+      </main>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
