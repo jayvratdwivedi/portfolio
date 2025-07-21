28c5d7d892ac8053c02b45b2603bdef88b02829d
Jayvrat Dwivedi <jayvratdwivedi202@gmail.com>
2025-07-21 21:00:42 +0530

    trying to fix, can be reverted

--- components/main/Hero.jsx
+++ components/main/Hero.jsx
@@ -12,7 +12,10 @@ const Hero = () => {
         loop
         className="rotate-180 absolute top-[-340px] left-0 w-full h-full object-cover -z-20"
       >
-        <source src="/videos/blackhole.webm" type="video/webm" />
+        <source
+          src={`${import.meta.env.BASE_URL}/videos/blackhole.webm`}
+          type="video/webm"
+        />
       </video>
       <HeroContent />
     </div>
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-const repoName = "portfolio";
-
 // https://vite.dev/config/
 export default defineConfig({
-  base: `/${repoName}/`,
+  base: "/portfolio/",
   plugins: [tailwindcss(), react()],
 });
