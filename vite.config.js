4487e62481a8e86847380093218d1396de7fc24b
Jayvrat Dwivedi <jayvratdwivedi202@gmail.com>
2025-07-21 20:51:36 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+const repoName = "portfolio";
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [tailwindcss(), react()],
 });
