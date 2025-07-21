685cf41cb2d0de465b5ebbac1201f6d628c87c19
Jayvrat Dwivedi <jayvratdwivedi202@gmail.com>
2025-07-21 22:11:02 +0530

    fix: accidental ios webkit in-app browsers full screen video playback

--- components/main/Hero.jsx
+++ components/main/Hero.jsx
@@ -10,6 +10,7 @@ const Hero = () => {
         autoPlay
         muted
         loop
+        playsInline
         className="rotate-180 absolute top-[-340px] left-0 w-full h-full object-cover -z-20"
       >
         <source src="./videos/blackhole.webm" type="video/webm" />
